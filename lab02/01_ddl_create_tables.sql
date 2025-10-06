88f8ae449b00896a5b524805c3c561327dd7fc3c
Ernesto Rios <ernestorios@outlook.com.pe>
2025-10-06 14:30:14 -0500

    Update create tables

--- lab02/01_ddl_create_tables.sql
+++ lab02/01_ddl_create_tables.sql
@@ -1,8 +1,12 @@
 USE testing;
 
-CREATE TABLE orders (
-	order_id INT,
-	customer_id INT,
-	order_date DATE,
-	order_amount DECIMAL(10, 2)
-);
+DROP TABLE IF EXISTS orders;
+
+IF OBJECT_ID(N'dbo.orders', N'U') IS NULL
+BEGIN
+	CREATE TABLE orders (
+		order_id INT,
+		customer_id INT,
+		order_date DATE,
+		order_amount DECIMAL(10, 2))
+END;
